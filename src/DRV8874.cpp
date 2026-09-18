1b764e95ddee2fdc99a442bd3041165e495d18bf
lillywho <lillywho@outlook.de>
2026-09-18 23:04:29 +0200

    Fix compile

--- src/DRV8874.cpp
+++ src/DRV8874.cpp
@@ -1,27 +1,51 @@
+// Depends on: https://github.com/rambros3d/ESP32_PWM_Fusion
+
 #include "DRV8874.h"
 
-DRV8874::DRV8874(uint8_t in1Pin, uint8_t in2Pin, uint8_t sleepPin) 
-    : _in1Pin(in1Pin), _in2Pin(in2Pin), _sleepPin(sleepPin) {}
-
+#include <Servo.h>
+Servo myservo = Servo();
+DRV8874::DRV8874(uint8_t in1Pin, uint8_t in2Pin) : _in1Pin(in1Pin), _in2Pin(in2Pin) {}
+// https://www.pololu.com/product/4035
 void DRV8874::begin() {
     pinMode(_in1Pin, OUTPUT);
     pinMode(_in2Pin, OUTPUT);
-    pinMode(_sleepPin,OUTPUT); // I wrote "pingMode" what the slobbering feck
     coast(); // Initialize in coast mode
 }
 
 void DRV8874::coast() {
+	if(myservo.attached(_in1Pin)) {
+		myservo.detach(_in1Pin);
+	}
+	if(myservo.attached(_in2Pin)) {
+		myservo.detach(_in2Pin);
+	}
     setOutputs(LOW, LOW);
 }
 
 void DRV8874::forward(uint8_t pwm) {
-    analogWrite(_in1Pin, pwm);
+	//reset all the pin mappings so we're free to write in forward mode
+	if(myservo.attached(_in1Pin)) {
+		myservo.detach(_in1Pin);
+	}
+	if(myservo.attached(_in2Pin)) {
+		myservo.detach(_in2Pin);
+	}
+	myservo.attach(_in1Pin);
+    myservo.writePwm(_in1Pin,pwm,10000,12);
     digitalWrite(_in2Pin, LOW);
 }
 
 void DRV8874::reverse(uint8_t pwm) {
+	//reset all the pin mappings so we're free to write in forward mode
+	if(myservo.attached(_in1Pin)) {
+		myservo.detach(_in1Pin);
+	}
+	if(myservo.attached(_in2Pin)) {
+		myservo.detach(_in2Pin);
+	}
+    myservo.attach(_in2Pin);
+    myservo.writePwm(_in2Pin,pwm,10000,12);
     digitalWrite(_in1Pin, LOW);
-    analogWrite(_in2Pin, pwm);
 }
 
 void DRV8874::brakeLow() {
@@ -29,25 +53,30 @@ void DRV8874::brakeLow() {
 }
 
 void DRV8874::brakeForward(uint8_t pwm) {
-    analogWrite(_in1Pin, pwm);
+	if(myservo.attached(_in1Pin)) {
+		myservo.detach(_in1Pin);
+	}
+	if(myservo.attached(_in2Pin)) {
+		myservo.detach(_in2Pin);
+	}
+    myservo.attachInvert(_in1Pin);
+    myservo.writePwm(_in1Pin,255 - pwm,10000,12);
     digitalWrite(_in2Pin, HIGH);
 }
 
 void DRV8874::brakeReverse(uint8_t pwm) {
+	if(myservo.attached(_in1Pin)) {
+		myservo.detach(_in1Pin);
+	}
+	if(myservo.attached(_in2Pin)) {
+		myservo.detach(_in2Pin);
+	}
+    myservo.attachInvert(_in2Pin);
+    myservo.writePwm(_in2Pin,255 - pwm,10000,12);
     digitalWrite(_in1Pin, HIGH);
-    analogWrite(_in2Pin, pwm);
 }
 
 void DRV8874::setOutputs(uint8_t in1, uint8_t in2) {
     digitalWrite(_in1Pin, in1);
     digitalWrite(_in2Pin, in2);
-}
-uint8_t DRV8874::positivePwm(int8_t Pwm_in) {
-    return (Pwm_in < 0) ? -Pwm_in : Pwm_in;
-}
-void DRV8874::doSleep(bool doSleep) {
-
-    digitalWrite(_sleepPin,!doSleep); // the SLEEP pin defaults to low, and the board sleeps if it is low, so if sleep == true then set the pin to FALSE
-    return;
-    
 }
--- src/DRV8874.h
+++ src/DRV8874.h
@@ -1,27 +1,30 @@
+#pragma once
 #ifndef DRV8874_H
 #define DRV8874_H
 
 #include <Arduino.h>
 
 class DRV8874 {
+
 public:
-    DRV8874(uint8_t in1Pin, uint8_t in2Pin, uint8_t sleepPin);
+	DRV8874(uint8_t in1Pin, uint8_t in2Pin);
 
-    void begin();
-    void coast();
-    void forward(uint8_t pwm);
-    void reverse(uint8_t pwm);
-    void brakeLow();
-    void brakeForward(uint8_t pwm);
-    void brakeReverse(uint8_t pwm);
-    void sleep(bool doSleep);
+	void begin();
+	void coast();
+	void forward(uint8_t pwm);
+	void reverse(uint8_t pwm);
+	void brakeLow();
+	void brakeForward(uint8_t pwm);
+	void brakeReverse(uint8_t pwm);
 
+uint8_t positivePwm(int8_t Pwm_in);
 private:
 
-    uint8_t _in1Pin, _in2Pin;
-    uint8_t _sleepPin;
-    uint8_t positivePwm(int8_t Pwm_in);
-    void setOutputs(uint8_t in1, uint8_t in2);
+	uint8_t _in1Pin;
+	uint8_t _in2Pin;
+	uint8_t _sleepPin;
+	
+	void setOutputs(uint8_t in1, uint8_t in2);
 };
 
 #endif
